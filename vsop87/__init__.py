c5c0947bc6d731c55d065899aa81a60c4940d2f2
Holmes Conan <holmesconan@gmail.com>
2020-08-09 23:32:47 +0800

    Add VSOP87 reference article url.

---
+++ vsop87/__init__.py
@@ -0,0 +1,6 @@
+"""VSOP87D 计算节气
+参考：
+https://blog.csdn.net/orbit/article/details/7825004
+https://blog.csdn.net/orbit/article/details/7910220
+https://blog.csdn.net/orbit/article/details/7944248
+"""
